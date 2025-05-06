220576af41cae881ae71f7721a90e5c8febc84e7
k-yoko <f.cloud.0951@gmail.com>
2025-05-06 19:20:21 +0900

    ci: exclude e2e folder from Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    // node_modules と e2e フォルダを除外
+    exclude: ["**/node_modules/**", "e2e/**"],
     environment: "jsdom",
     globals: true,
   },
